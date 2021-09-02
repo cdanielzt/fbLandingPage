7753513c223252f2352f1f286c9ed3649497f82e
Carlos Zapata <cdanielzt@gmail.com>
2021-09-01 19:19:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function(){
+    openMenu();
+});
+
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+console.log(slides);
+console.log(dots);
+function setActive(i){
+    for(slide of slides)
+        slide.classList.remove('active');
+        slides[i].classList.add('active');
+
+    for(dot of dots)
+        dot.classList.remove('active');
+        dots[i].classList.add('active');
+}
+
+for(let i=0; i < dots.length; i++){
+    dots[i].addEventListener('click', function(){
+        setActive(i);
+    });
+}
+
+
+function openMenu() {
+    var hamburguer = document.getElementById('hamburguer');
+
+    hamburguer.addEventListener('click', function(){
+        var list = document.getElementById("links");
+        if (list.classList.contains('menu-mobile')) {
+        list.classList.remove('menu-mobile')
+        } else {
+            list.classList.add('menu-mobile')
+        }
+    });
+    
+  }
